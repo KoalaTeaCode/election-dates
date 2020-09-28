d7c56403ba23df14c9e17393e04d7afc3c90ada0
Keith Holliday <keithrholliday@gmail.com>
2020-09-28 16:24:52 -0600

    Initial refactor

--- main.js
+++ main.js
@@ -1,52 +1,87 @@
-var fs = require("fs");
-var request = require("request");
-var cheerio = require("cheerio");
+const fs = require("fs");
+const request = require("request");
+const cheerio = require("cheerio");
 
-var fileOutput = "";
-fileOutput += "Subject, Start Date\n";
+const states = require('./data/states.json')
 
-var url = "https://www.usvotefoundation.org/vote/state-elections/state-election-dates-deadlines.htm";
-request(url, function(error, response, html) {
-  var $ = cheerio.load(html);
+const url = "https://www.usvotefoundation.org/vote/state-elections/state-election-dates-deadlines.htm?stateName=";
 
-  $('.main-content').each(function(){
-    var rows = $(this).children().children().children();
-    var stateName = "UNKNOWN";
-    rows.each(function(){
-      $(this).children().filter(".state-name").first().children().filter("h1").each(function(){
-        stateName = $(this).text().replace(" (more info)", "");
-        console.log(stateName);
-      })
-      var title = "UNKNOWN";
-      $(this).children().filter(".election-title").children().filter("h2").each(function(){
-        var text = $(this).text();
-        var split = text.split(" - ");
-        var title = split[0];
-        var date = new Date(split[1]);
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var year = date.getFullYear();
+function saveElectionData(fileOutput) {
+  fs.writeFileSync('election-calendar.csv', fileOutput);
+}
 
-        var formattedDate = month + "/" + day + "/" + year;
-        //subject, start date
-        var rowText = "\"" + stateName + ": " + title + "\"," + formattedDate;
-        console.log("   " + title);
-        console.log("   " + date);
-        console.log("     " + rowText);
-        console.log("   ");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-        fileOutput += rowText + "\n";
 
-      })
+const rp = function (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function(error, response, html) {
+      if (error) return reject(error)
+      return resolve({response, html})
     })
   })
-  console.log(fileOutput);
-  fs.writeFile('election-calendar.csv', fileOutput, function(err){
-    if(!err){
+}
 
-      console.log('success');
-    } else {
-      console.log(err);
-    }
+function parseHTML(html) {
+  let fileOutput = "";
+  fileOutput += "Subject, Start Date\n";
+
+  const $ = cheerio.load(html);
+  
+  let stateName = "UNKNOWN";
+
+  function parseElectionHeader() {
+      const text = $(this).text();
+      const split = text.split(" - ");
+      const title = split[1] || "UNKNOWN";
+      const date = new Date(split[0]);
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const year = date.getFullYear();
+
+      const formattedDate = month + "/" + day + "/" + year;
+      const rowText = "\"" + stateName + ": " + title + "\"," + formattedDate;
+
+      fileOutput += rowText + "\n";
+  }
+
+  function parseRow() {
+      $(this)
+        .children().filter(".state-name").first()
+        .children().filter("h1")
+        .each(function(){
+          stateName = $(this).text().replace(" (more info)", "");
+        });
+
+      const electionTitle = $(this).children().filter(".election-title")
+      const electionHeaders =  electionTitle.children().filter("h2");
+      
+      electionHeaders.each(parseElectionHeader);
+  }
+
+  $('.main-content').each(function () {
+    const rows = $(this).children().children().children();
+    rows.each(parseRow);
   })
-})
+
+  return fileOutput;
+}
+
+async function init() {
+  const electionOutputs = []
+  for (const state of states) {
+    console.log(`Processing state ${state.State}`)
+    const {_, html} = await rp(`${url}${state.Code}`);
+    const fileOutput = parseHTML(html);
+    electionOutputs.push(fileOutput);
+    await sleep(1000);
+  }
+  
+  saveElectionData(electionOutputs.join("\n"));
+}
+
+(async () => {
+  await init()
+})()
